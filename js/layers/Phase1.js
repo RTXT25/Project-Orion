a3d69e7d2154857056a5922a249c02d66c069562
Exterminate5573 <exterminate5573@gmail.com>
2021-11-18 10:01:07 +1100

    Update Phase1.js

--- js/layers/Phase1.js
+++ js/layers/Phase1.js
@@ -175,7 +175,7 @@ addLayer("c", {
     symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return  {
-        unlocked: true,
+        unlocked: false,
 		points: new Decimal(0),
         parts: new Decimal(0)
     }},
